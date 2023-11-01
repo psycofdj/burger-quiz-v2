04f8d1cce4dddb541a9ed541f81ac8aae9e6cc05
Xavier MARCELET <xavier@marcelet.com>
2023-11-01 16:43:06 +0100

    test1

--- bquiz/gui.py
+++ bquiz/gui.py
@@ -106,7 +106,7 @@ class AdditionRenderer:
         lastText = label.text()
         label.setText(text)
         label.adjustSize()
-        log("label.width(): %d" % label.width())
+        label.updateGeometry()
         log("label.height() < self.maxHeight: %d < %d" % (
             label.height(),
             self.maxHeight,

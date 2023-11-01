69e2f2692b4836d00a1d9e6f669c92f095c3433a
Xavier MARCELET <xavier@marcelet.com>
2023-11-01 19:08:03 +0100

    test1

--- bquiz/gui.py
+++ bquiz/gui.py
@@ -37,54 +37,73 @@ def log(arg):
     with open("/tmp/log", "a") as f:
         f.write(str(arg) + "\n")
 
-class AdditionRenderer:
+class DefaultRenderer:
     def __init__(self, item, initialPos):
         self.initialPos = initialPos
         self.title = item.get("title", "")
         self.subtitle = item.get("subtitle", None)
         self.questions = item.get("questions", [])
         self.size = item.get("size", "normal")
-        self.maxHeight = 400
-    @staticmethod
-    def formatTitle(title, subtitle):
-        if subtitle:
-            return """<b>%s</b><br/><i>%s</i>""" % (title, subtitle)
-        return """<b>%s</b>""" % (title)
+        self.maxHeight = 406
 
-    @staticmethod
-    def formatQuestion(qItem):
-        q = qItem["Q"]
-        a = qItem["A"]
-        return """<li> %(Q)s.<br/><span class="answer">%(A)s</span></li>""" % qItem
+    def renderTitle(self):
+        if self.initialPos != 0 or not self.title:
+            # no title for back page
+            return ""
+        inner = """<b>%s</b>""" % (self.title)
+        if self.subtitle:
+            inner = """<b>%s</b><br/><i>%s</i>""" % (self.title, self.subtitle)
+        return """<div class="title">%(inner)s</div>""" % {
+            "inner": inner
+        }
 
-    def formatText(self, questions):
+    def renderQuestion(self, qItem):
+        q = qItem.get("Q", "<missing>")
+        a = qItem.get("A", "<missing>")
+        return """
+        <li>
+          %(Q)s.<br/><span class="answer">%(A)s</span>
+        </li>""" % qItem
+
+    def renderQuestions(self, questions):
+        return """
+        <div class="q">
+          <ul>
+          %(q)s
+          </ul>
+        </div>""" % {
+            "q": "\n".join([ self.renderQuestion(x) for x in questions ])
+        }
+
+    def renderStyles(self):
         sizes = { "title": "28px", "q": "25px" }
         if self.size == "small":
             sizes = { "title": "22px", "q": "18px" }
         if self.size == "medium":
             sizes = { "title": "26px", "q": "22px" }
-        q = """<div class="q"><ul> %(q)s </ul></div>""" % {
-            "q": "\n".join([ self.formatQuestion(x) for x in questions ])
-        }
-        t = """<div class="title">%(title)s</div>""" % {
-            "title": self.formatTitle(self.title, self.subtitle)
-        }
-        # no title for back page
-        if self.initialPos != 0:
-            t = ""
         return """
-        <style>
+         <style>
           .title  { font-size: %(sizes.title)s; }
           .q      { font-size: %(sizes.q)s; }
           .answer { color: red; }
         </style>
-        %(title)s
-        %(questions)s
         """ % {
             "sizes.title": sizes['title'],
             "sizes.q": sizes['q'],
-            "title": t,
-            "questions": q,
+        }
+
+    def renderChunk(self, chunk):
+        styles = self.renderStyles()
+        title = self.renderTitle()
+        questions = self.renderQuestions(chunk)
+        return """
+        %(styles)s
+        %(title)s
+        %(questions)s
+        """ % {
+            "styles": styles,
+            "title": title,
+            "questions": questions,
         }
 
     @staticmethod
@@ -99,25 +118,19 @@ class AdditionRenderer:
         label.setAlignment(QtCore.Qt.AlignVCenter | QtCore.Qt.AlignLeft)
         return label
 
-    def nextTextValid(self, label, text):
+    def isValid(self, label, text):
         doc = QtGui.QTextDocument(None)
         doc.setHtml(text)
         doc.setDefaultFont(label.font())
         doc.setTextWidth(800 - 55 - 135)
         size = doc.documentLayout().documentSize()
-        log("size.height() > self.maxHeight: %d < %d" % (
-            size.height(),
-            self.maxHeight,
-        ))
         if size.height() > self.maxHeight:
             return False
         label.setText(text)
         return True
 
     def finalize(self, label, pos):
-        # log("pos: %d" % pos)
         hasNext = (pos < len(self.questions) - 1)
-        # log("hasNext: %d" % hasNext)
         return label, hasNext, pos
 
     def chunkQ(self, label):
@@ -129,51 +142,200 @@ class AdditionRenderer:
             maxIdx = maxIdx/2
         while qIdx+qCount < maxIdx:
             qCount += 1
-            log("try: [%d:%d]" % (qIdx,qIdx+qCount))
             qSubset = self.questions[qIdx:qIdx+qCount]
-            text = self.formatText(qSubset)
-            if not self.nextTextValid(label, text):
-                log("rejected")
+            text = self.renderChunk(qSubset)
+            if not self.isValid(label, text):
                 break
         return self.finalize(label, qIdx+qCount)
 
     def render(self):
         label = self.createLabel()
-        text = self.formatText(self.questions)
-        if self.initialPos != 0 or not self.nextTextValid(label, text):
+        text = self.renderChunk(self.questions)
+        if self.initialPos != 0 or not self.isValid(label, text):
             return self.chunkQ(label)
-        log("one chunk !")
         return self.finalize(label, len(self.questions) - 1)
 
 
+class AdditionRenderer(DefaultRenderer):
+    def __init__(self, item, initialPos):
+        DefaultRenderer.__init__(self, item, initialPos)
+
+class SelPoivreRenderer(DefaultRenderer):
+    def __init__(self, item, initialPos):
+        DefaultRenderer.__init__(self, item, initialPos)
+
+class NuggetsRenderer(DefaultRenderer):
+    def __init__(self, item, initialPos):
+        DefaultRenderer.__init__(self, item, initialPos)
+
+    def renderStyles(self):
+        sizes = { "q": "22px" }
+        return """
+         <style>
+          .q      { font-size: %(sizes.q)s; }
+          .answer { color: red; }
+         </style>
+        """ % {
+            "sizes.q": sizes['q'],
+        }
+
+    def renderChoice(self, value, isAnswer):
+        itemClass = ""
+        if isAnswer:
+            itemClass = "answer"
+        return """
+          <li class="%(class)s">%(value)s</li>
+        """ % {
+            'class': itemClass,
+            'value': value,
+        }
+
+    def renderQuestion(self, qItem, idx):
+        choices = ['A', 'B', 'C', 'D']
+        answer = qItem.get('answer', None)
+        def isValid(choice):
+            if type(answer) == list:
+                return choice in answer
+            return choice == answer
+        items = [ self.renderChoice(qItem['choices'].get(x, ""), isValid(x)) for x in choices ]
+        return """
+        <li>
+          <b>%(idx)s. %(Q)s</b>
+          <ul>
+            %(items)s
+          </ul>
+        </li>
+        """ % {
+            'idx': self.initialPos + idx + 1,
+            'Q': qItem.get('question', ""),
+            'items': "\n".join(items),
+        }
+
+    def renderQuestions(self, questions):
+        return """
+        <div class="q">
+        %(q)s
+        </div>""" % {
+            "q": "<br/>".join([ self.renderQuestion(val, idx) for idx,val in enumerate(questions) ])
+        }
+
+    def render(self):
+        label = self.createLabel()
+        if self.initialPos == 0:
+            questions = self.questions[0:2]
+        else:
+            questions = self.questions[2:4]
+        text = self.renderChunk(questions)
+        label.setText(text)
+        return self.finalize(label, self.initialPos + 2)
+
+class MenusRenderer(DefaultRenderer):
+    def __init__(self, item, initialPos):
+        DefaultRenderer.__init__(self, item, initialPos)
+        self.sizes = [ "normal", "medium", "small", "xsmall" ]
+
+    def renderStyles(self):
+        sizes = { "title": "28px", "q": "25px" }
+        if self.size == "medium":
+            sizes = { "title": "26px", "q": "22px" }
+        if self.size == "small":
+            sizes = { "title": "22px", "q": "18px" }
+        if self.size == "xsmall":
+            sizes = { "title": "22px", "q": "16px" }
+        return """
+         <style>
+          .title  { font-size: %(sizes.title)s; }
+          .q      { font-size: %(sizes.q)s; }
+          .answer { color: red; }
+        </style>
+        """ % {
+            "sizes.title": sizes['title'],
+            "sizes.q": sizes['q'],
+        }
+
+    def render(self):
+        idx = 0
+        label = self.createLabel()
+        text = self.renderChunk(self.questions)
+        while not self.isValid(label, text) and idx < len(self.sizes):
+            idx += 1
+            self.size = self.sizes[idx]
+            text = self.renderChunk(self.questions)
+        label.setText(text)
+        return self.finalize(label, len(self.questions) - 1)
+
 class MyWindow(QtWidgets.QMainWindow):
     def __init__(self):
-        path = os.path.join(os.path.dirname(__file__), "resources", "addition.yaml")
+        self.config = {
+            "nuggets" : {
+                "bg-color": "#ffff8d",
+                "file": "nuggets.yaml",
+                "renderer": NuggetsRenderer,
+                "bg": 'nuggets-back.png',
+                "bg-arrow": 'nuggets-back.png',
+            },
+            "selpoivre": {
+                "bg-color": "#d4bee4",
+                "file": "selpoivre.yaml",
+                "bg": 'selpoivre-back.png',
+                "bg-arrow": 'selpoivre-back-arrow.png',
+                "renderer": SelPoivreRenderer,
+            },
+            "menus": {
+                "bg-color": "#cfffff",
+                "file": "menus.yaml",
+                "renderer": MenusRenderer,
+                "bg": 'menus-back.png',
+                "bg-arrow": 'menus-back-arrow.png',
+            },
+            "menus-rouge": {
+                "bg-color": "#ffd38f",
+                "file": "menus-rouge.yaml",
+                "renderer": MenusRenderer,
+                "bg": 'menus-rouge-back.png',
+                "bg-arrow": 'menus-rouge-back.png',
+            },
+            "addition": {
+                "bg-color": "#ff9d82",
+                "file": "addition.yaml",
+                "renderer": AdditionRenderer,
+                "bg": 'addition-back.png',
+                "bg-arrow": 'addition-back-arrow.png',
+            }
+        }
+        self.current = "menus-rouge"
+        path = os.path.join(os.path.dirname(__file__), "resources", self.config[self.current]["file"])
         with open(path) as f:
-            self.additions = yaml.safe_load(f)
+            self.data = yaml.safe_load(f)
         QtWidgets.QMainWindow.__init__(self)
         self.qIndex = 0
         self.qPos = 0
         self.setFixedWidth(800)
         self.setFixedHeight(480)
+        self.setBackgroundImage(False)
         self.setQ(self.qIndex, self.qPos)
 
-    def setBackgroundImage(self, name):
+    def setBackgroundImage(self, arrow):
+        name = self.config[self.current]['bg']
+        if arrow:
+            name = self.config[self.current]['bg-arrow']
         path = os.path.join(os.path.dirname(__file__), "resources", name)
-        # selpouvre: d4bee4
-        # addition: ff9d82
-        self.setStyleSheet("""
-QMainWindow {
-  border-image: url(\"%s\");
-  background-color: #ff9d82;
-}
-QLabel {
-  margin: 40px 55px 43px 135px;
-}
-""" % path);
+        style = """
+        QMainWindow {
+          border-image: url("%(path)s");
+          background-color: %(bg-color)s;
+        }
+        QLabel {
+          margin: 40px 55px 43px 135px;
+        }""" % {
+            'path': path,
+            'bg-color': self.config[self.current]['bg-color'],
+        }
+        self.setStyleSheet(style)
 
     def nextQ(self):
         self.setQ(self.nextIndex, self.nextPos)
+
     def prevQ(self):
         self.setQ(self.prevIndex, self.prevPos)
 
@@ -185,18 +347,15 @@ QLabel {
             self.prevPos = 0
         self.qIndex = index
         self.qPos = pos
-        print("displaying question %d, backpage ? %d" % (self.qIndex+1, self.qPos != 0))
-        renderer = AdditionRenderer(self.additions[self.qIndex], self.qPos)
+        renderer = self.config[self.current]['renderer'](self.data[self.qIndex], self.qPos)
         self.label, hasNext, nextPos = renderer.render()
         if hasNext:
-            # self.setBackgroundImage("addition-back-arrow.png")
-            self.setBackgroundImage("addition-back-arrow.png")
+            self.setBackgroundImage(True)
             self.nextIndex = self.qIndex
             self.nextPos = nextPos
         else:
-            # self.setBackgroundImage("addition-back.png")
-            self.setBackgroundImage("addition-back.png")
-            if (self.qIndex + 1) != len(self.additions):
+            self.setBackgroundImage(False)
+            if (self.qIndex + 1) != len(self.data):
                 self.nextIndex = self.qIndex + 1
                 self.nextPos = 0
 
@@ -205,10 +364,10 @@ QLabel {
         self.pageNum.setMinimumHeight(110)
         self.pageNum.setMinimumWidth(75)
         self.pageNum.move(630, 400)
-        self.pageNum.setStyleSheet("QLabel { color: #d4bee4 }")
+        #self.pageNum.setStyleSheet("QLabel { color: #d4bee4 }")
         self.label.setParent(self)
         self.setCentralWidget(self.label)
-
+        self.setWindowFlag(Qt.FramelessWindowHint)
 
     def keyPressEvent(self, event):
         if event.key() == Qt.Key_Escape:
